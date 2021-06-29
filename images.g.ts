5c112d5968cc598df21ecc5fd1ef2d08a08cb0d0
jstocksf <86687988+jstocksf@users.noreply.github.com>
2021-06-29 15:49:46 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "}o$*Zky;H!zM=S$PF2VS":
+            case "Scuba Steve":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . f f d . . . 
+. . d . . . 5 5 5 5 f d 1 e . . 
+. . d f f f f f f f f 5 1 e . . 
+. . . . . . f f f f f d 1 e . . 
+. . d f f f f f f f f f d . . . 
+. . d . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
